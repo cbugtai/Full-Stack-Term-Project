b15fb470ff118ed3721c8e277752d619e1bd8b21
pinyirao <prao2@rrc.ca>
2025-09-06 02:11:23 -0500

    feat: finish product list component and the corresponding styling development

---
+++ src/components/product-list/ProductList.tsx
@@ -0,0 +1,60 @@
+import type { JSX } from "react";
+import { products } from "./sample-data/sample-data";
+import type { Product } from "./sample-data/sample-data";
+import "./ProductList.css";
+
+function ProductList() {
+  const productList: JSX.Element[] = [];
+  products.forEach((p, i) =>
+    productList.push(<ProductCard product={p} key={i} />)
+  );
+
+  return (
+    <>
+      <section className="product-list">
+        <h2>Featured Products</h2>
+        <div className="product-list-gallery">{productList}</div>
+      </section>
+    </>
+  );
+}
+
+function ProductCard({ product }: { product: Product }) {
+  // use the seed to generate random image
+  const randomImgUrl = `https://picsum.photos/seed/${Math.random()}/165`;
+
+  return (
+    <div className="product-card">
+      <img
+        src={randomImgUrl}
+        alt="product image"
+        // to make sure could load every img
+        onError={(e) => {
+          // assert the target type
+          const target = e.target as HTMLImageElement;
+          target.src = randomImgUrl;
+        }}
+      />
+      <div className="product-desc">{product.description}</div>
+      <div className="product-brand">
+        <strong>Brand:</strong>
+        {` ${product.brand}`}
+      </div>
+      <div className="product-condition">
+        <strong>Condition: </strong>
+        {`${product.condition}`}
+      </div>
+      <div>
+        <span className="cur-price">{`$${product.currentPrice.toFixed(
+          2
+        )}`}</span>
+        {"  "}
+        <span className="original-price">
+          <del>{`Was $${product.originalPrice.toFixed(2)}`}</del>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
