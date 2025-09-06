355b9b2cba4cf4edc6d3a8b31122c186e8d55e9e
pinyirao <prao2@rrc.ca>
2025-09-06 14:18:56 -0500

    feat: add the global color paletter configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/common/header/Header";
 import Footer from "./components/common/footer/Footer";
+import NavBar from "./components/nav/NavBar";
 import ProductList from "./components/product-list/ProductList";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <main>
+        <NavBar />
         {/* render the featured product list */}
         <ProductList />
       </main>
