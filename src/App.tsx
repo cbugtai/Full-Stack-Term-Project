54963f73122ca0c4f3b320c26bfdd2461f6fc983
pinyirao <prao2@rrc.ca>
2025-09-06 13:37:15 -0500

    feat: finish header and footer component dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import Header from "./components/common/header/Header";
+import Footer from "./components/common/footer/Footer";
 import ProductList from "./components/product-list/ProductList";
 
 function App() {
   return (
     <>
+      <Header />
       <main>
         {/* render the featured product list */}
         <ProductList />
       </main>
+      <Footer />
     </>
   );
 }
