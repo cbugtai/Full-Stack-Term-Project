aebe083c0ef4bb7936922486522b023a12ae00af
cbugtai <cbugtai@academic.rrc.ca>
2025-09-29 13:56:12 -0500

    Moved app.tsx <main> render to Landing.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Header from "./components/common/layout/header/Header";
 import Footer from "./components/common/layout/footer/Footer";
-import ProductList from "./components/product-list/ProductList";
-import TopSellers from "./components/top-sellers/TopSellers";
-import Testimonials from "./components/testimonials/testimonials";
+import ProductList from "./components/pages/landing/product-list/ProductList";
+import TopSellers from "./components/pages/landing/top-sellers/TopSellers";
+import Testimonials from "./components/pages/landing/testimonials/testimonials";
 
 function App() {
   return (
