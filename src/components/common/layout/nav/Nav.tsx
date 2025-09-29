9cb451ccb2b980170a4dafb6ac1cd58d2469174f
cbugtai <cbugtai@academic.rrc.ca>
2025-09-29 13:41:00 -0500

    Created Layout.tsx component that contains the '0utlet' component

---
+++ src/components/common/layout/nav/Nav.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
+
+function Nav() {
+    return(<nav>
+         <div className="page-links">
+            <NavLink to="/" end>
+                Home
+            </NavLink>
+            <NavLink to="/page1">
+                Page 1
+            </NavLink>
+            <NavLink to="/page2">
+                Page 2
+            </NavLink>
+            <NavLink to="/page3">
+                Page 3
+            </NavLink>
+        </div>
+    </nav>);
+}
+
+export default Nav;
