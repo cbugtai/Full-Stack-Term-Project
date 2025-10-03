b931c1251bdecb8b40613aad43502d03969103c0
pinyirao <prao2@rrc.ca>
2025-10-03 09:04:07 -0500

    refactor: make the independant reviewfillform component

--- src/components/common/product-list/ProductList.tsx
+++ src/components/common/product-list/ProductList.tsx
@@ -1,5 +1,6 @@
 import type { Product } from "./sample-data/sample-data";
 import ProductCard from "./ProductCard";
+import ReviewFillForm from "./ReviewFillForm";
 import React from "react";
 
 function ProductList({
@@ -42,67 +43,6 @@ function ProductList({
         ) : null}
       </section>
     </>
-  );
-}
-
-function ReviewFillForm({
-  id,
-  description,
-  updateProducts,
-}: {
-  id: number;
-  description: string;
-  updateProducts: React.Dispatch<React.SetStateAction<Product[]>>;
-}) {
-  const [comment, setComment] = React.useState("");
-  const [isValid, setIsValid] = React.useState(true);
-
-  return (
-    <form
-      className="review-fill-form"
-      onSubmit={(e) => {
-        e.preventDefault();
-        // validate
-        if (comment.length <= 10) {
-          setIsValid(false);
-          return;
-        }
-
-        // update the reivew
-        updateProducts((prev) =>
-          prev.map((p) =>
-            p.id === id
-              ? {
-                  ...p,
-                  reviews: [
-                    ...(p.reviews ?? []),
-                    { user: "Anonymous User", comment: comment },
-                  ],
-                }
-              : p
-          )
-        );
-      }}
-    >
-      <input type="text" value={description} />
-      <textarea
-        placeholder="Your Review"
-        rows={4}
-        cols={50}
-        value={comment}
-        onChange={(e) => {
-          setIsValid(true);
-          setComment(e.target.value);
-        }}
-        required
-      ></textarea>
-      {!isValid ? (
-        <p className="validation-error">
-          Review must be more than 10 characters.
-        </p>
-      ) : null}
-      <button type="submit">Submit Review</button>
-    </form>
   );
 }
 
---
+++ src/components/common/product-list/ReviewFillForm.tsx
@@ -0,0 +1,65 @@
+import type { Product } from "./sample-data/sample-data";
+import React from "react";
+
+function ReviewFillForm({
+  id,
+  description,
+  updateProducts,
+}: {
+  id: number;
+  description: string;
+  updateProducts: React.Dispatch<React.SetStateAction<Product[]>>;
+}) {
+  const [comment, setComment] = React.useState("");
+  const [isValid, setIsValid] = React.useState(true);
+
+  return (
+    <form
+      className="review-fill-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        // validate
+        if (comment.length <= 10) {
+          setIsValid(false);
+          return;
+        }
+
+        // update the reivew
+        updateProducts((prev) =>
+          prev.map((p) =>
+            p.id === id
+              ? {
+                  ...p,
+                  reviews: [
+                    ...(p.reviews ?? []),
+                    { user: "Anonymous User", comment: comment },
+                  ],
+                }
+              : p
+          )
+        );
+      }}
+    >
+      <input type="text" value={description} />
+      <textarea
+        placeholder="Your Review"
+        rows={4}
+        cols={50}
+        value={comment}
+        onChange={(e) => {
+          setIsValid(true);
+          setComment(e.target.value);
+        }}
+        required
+      ></textarea>
+      {!isValid ? (
+        <p className="validation-error">
+          Review must be more than 10 characters.
+        </p>
+      ) : null}
+      <button type="submit">Submit Review</button>
+    </form>
+  );
+}
+
+export default ReviewFillForm;
