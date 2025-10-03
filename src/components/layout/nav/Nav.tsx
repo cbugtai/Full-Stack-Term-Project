9470bf2d8e1217711c325ea39a4dda948ea1427b
pinyirao <prao2@rrc.ca>
2025-10-03 05:09:32 -0500

    feature: add the wishlist page

--- src/components/layout/nav/Nav.tsx
+++ src/components/layout/nav/Nav.tsx
@@ -2,22 +2,18 @@ import { NavLink } from "react-router-dom";
 //import "./Nav.css";
 
 function Nav() {
-    return(<nav>
-         <div className="page-links">
-            <NavLink to="/" end>
-                Home
-            </NavLink>
-            <NavLink to="/page1">
-                Page 1
-            </NavLink>
-            <NavLink to="/sellers">
-                All Sellers
-            </NavLink>
-            <NavLink to="/page3">
-                Page 3
-            </NavLink>
-        </div>
-    </nav>);
+  return (
+    <nav>
+      <div className="page-links">
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/wishlist">Wish List</NavLink>
+        <NavLink to="/sellers">All Sellers</NavLink>
+        <NavLink to="/page3">Page 3</NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
