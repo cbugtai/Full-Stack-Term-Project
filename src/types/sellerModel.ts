15cc65f369beb85c1c9acfae68e8153d1d34cbbb
cbugtai <cbugtai@academic.rrc.ca>
2025-10-02 21:29:11 -0500

    updated sellers_list and sellerModel to include id

--- src/types/sellerModel.ts
+++ src/types/sellerModel.ts
@@ -1,4 +1,5 @@
 export type Seller = {
+    id: number;             //unique seller ID
     username: string;       //username
     rating: number;         //trustworthiness rating out of 100
     completed_sales: number //total number of sales completed
