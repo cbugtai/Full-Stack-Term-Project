c746b2794099637562e1a4e8034ac35b8a839c4a
cbugtai <cbugtai@academic.rrc.ca>
2025-10-02 22:48:48 -0500

    feat: Added search functionality on sellers page

--- src/components/pages/sellers/Sellers.tsx
+++ src/components/pages/sellers/Sellers.tsx
@@ -2,14 +2,26 @@ import { useState } from "react";
 import type { Seller } from "@/types/sellerModel";
 import sellerData from "@/data/sellers_list.json";
 import { SellersListDisplay } from "./sellersListDisplay/SellersListDisplay";
+import { Search } from "./search/Search";
 
 function Sellers() {
   const [sellers, setSellers] = useState<Seller[]>(sellerData);
+  const [searchValue, setSearchValue] = useState<string>("");
 
   return (
     <div>
-      <h1>List of all sellers</h1>
-      <SellersListDisplay sellers={sellers} />
+      <h1>Sellers</h1>
+
+      <Search
+        searchValue={searchValue}
+        handleSearchChange={setSearchValue}
+      />
+      
+      <SellersListDisplay sellers={
+        sellers.filter((seller) =>
+          seller.username.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      } />
     </div>
   );
 }
