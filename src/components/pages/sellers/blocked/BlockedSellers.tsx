60736d3044b45446b5c6b598ffee92f72e368df1
cbugtai <cbugtai@academic.rrc.ca>
2025-10-05 17:51:58 -0500

    Feat: Added List diplay on favorite and blocked sellers pages

--- src/components/pages/sellers/blocked/BlockedSellers.tsx
+++ src/components/pages/sellers/blocked/BlockedSellers.tsx
@@ -1,3 +1,20 @@
+import { SellersNav } from "@/components/common/sellers/sellers-nav/SellersNav";
+import { SellersListDisplay } from "@/components/common//sellers/sellersListDisplay/SellersListDisplay";
+import sellerData from "@/data/sellers_list.json";
+
 export function SellersBlocked() {
-  return <div>Blocked Sellers Page</div>;
+
+  return (
+    <div>
+      <SellersNav />
+      <h1>Blocked Sellers</h1>
+      
+      <SellersListDisplay sellers={
+        sellerData.filter((seller) =>
+          seller.isBlocked === true
+          )
+        }
+      />
+    </div>
+  );
 }
--- src/components/pages/sellers/favorites/FavoriteSellers.tsx
+++ src/components/pages/sellers/favorites/FavoriteSellers.tsx
@@ -1,3 +1,20 @@
+import { SellersNav } from "@/components/common/sellers/sellers-nav/SellersNav";
+import { SellersListDisplay } from "@/components/common//sellers/sellersListDisplay/SellersListDisplay";
+import sellerData from "@/data/sellers_list.json";
+
 export function SellersFavorites() {
-  return <div>Favorite Sellers Page</div>;
+
+  return (
+    <div>
+      <SellersNav />
+      <h1>Favorite Sellers</h1>
+      
+      <SellersListDisplay sellers={
+        sellerData.filter((seller) =>
+          seller.isFavorite === true
+          )
+        }
+      />
+    </div>
+  );
 }
