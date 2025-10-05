59a8d00417339423ab39690cf9c92bd6db954e58
cbugtai <cbugtai@academic.rrc.ca>
2025-10-05 17:17:59 -0500

    updated sample data to contain isFavorite and isBlocked fields

--- src/types/sellerModel.ts
+++ src/types/sellerModel.ts
@@ -4,4 +4,6 @@ export type Seller = {
     rating: number;         //trustworthiness rating out of 100
     completed_sales: number //total number of sales completed
     photo?: string;         //image/avatar link
+    isFavorite: boolean;    //whether the user has favorited this seller
+    isBlocked: boolean;     //whether the user has blocked this seller
 }
