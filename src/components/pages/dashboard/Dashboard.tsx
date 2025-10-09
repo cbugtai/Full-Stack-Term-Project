dc104c4dcb48abdf2360c1fd14dd74ff9a9d5a2c
Nick Gowler <ngowler@academic.rrc.ca>
2025-10-09 05:19:40 -0500

    Turn dashboard options into an interactive side bar.

--- src/components/pages/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,12 @@
-import { DashboardOptions } from "./options/DashboardOptions";
+import { Sidebar } from "./sidebar/Sidebar";
 import { DashboardDisplay } from "./display/DashboardDisplay";
 import "./Dashboard.css";
 
 export function Dashboard() {
     return (
         <div className="dashboard-container">
-
             <div className="dashboard-main">
-                <DashboardOptions />
+                <Sidebar />
                 <DashboardDisplay />
             </div>
         </div>
