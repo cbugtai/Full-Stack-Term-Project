df7f14f0c6f40f08dc4dbd2be5e17d7d56a0d608
Nick Gowler <ngowler@academic.rrc.ca>
2025-10-09 03:32:48 -0500

    Remove main dashboard header, add underline effect.

--- src/components/pages/dashboard/display/DashboardDisplay.tsx
+++ src/components/pages/dashboard/display/DashboardDisplay.tsx
@@ -1,3 +1,5 @@
+import BulbIcon from "@/assets/icons/BulbIcon.svg?react";
+import DocumentCheckIcon from "@/assets/icons/DocumentCheckIcon.svg?react";
 import { mockUser } from "../../../../apis/user/mockUserData";
 import "./DashboardDisplay.css";
 
@@ -13,12 +15,18 @@ export function DashboardDisplay() {
 
             <div className="dashboard-widgets">
                 <div className="widget">
-                    <h4>Recent Activity</h4>
+                    <div className="display-row">
+                        <DocumentCheckIcon className="icon" />
+                        <h3>Recent Activity</h3>
+                    </div>
                     <p>No recent changes. Start by updating your profile or viewing listings.</p>
                 </div>
 
                 <div className="widget">
-                    <h4>Quick Tips</h4>
+                    <div className="display-row">
+                        <BulbIcon className="icon" />
+                        <h3>Quick Tips</h3>
+                    </div>
                     <ul>
                         <li>Use strong passwords for better security</li>
                         <li>Keep your contact info up to date</li>
