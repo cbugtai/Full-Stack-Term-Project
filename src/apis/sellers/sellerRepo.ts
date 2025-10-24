6975d3001b0be9a97648a20573338a8f8cdd0262
cbugtai <cbugtai@academic.rrc.ca>
2025-10-24 10:01:10 -0500

    Updated sellerRepo to reflect sellerService

--- src/apis/sellers/sellerRepo.ts
+++ src/apis/sellers/sellerRepo.ts
@@ -5,22 +5,55 @@ export async function fetchAllSellers(): Promise<Seller[]> {
     return sellerData;
 }
 
-export async function toggleFavoriteSeller(sellerId: number): Promise<Seller | null> {
+export async function getSellerById(sellerId: number): Promise<Seller> {
     const seller = sellerData.find(s => s.id === sellerId);
-    if (!seller){
+    if (!seller) {
         throw new Error(`Seller with ID ${sellerId} not found`);
-    } else {
-        seller.isFavorite = !seller.isFavorite;
     }
-    return seller;
-}  
 
-export async function toggleBlockedSeller(sellerId: number): Promise<Seller | null> {
+    return seller;
+}
+
+export async function addFavoriteSeller(sellerId: number): Promise<Seller | null> {
     const seller = sellerData.find(s => s.id === sellerId);
     if (!seller){
         throw new Error(`Seller with ID ${sellerId} not found`);
     } else {
-        seller.isBlocked = !seller.isBlocked;
+        seller.isFavorite = true;
     }
+
+    return seller;
+}
+
+export async function removeFavoriteSeller(sellerId: number): Promise<Seller | null> {
+    const seller = sellerData.find(s => s.id === sellerId);
+    if (!seller){
+        throw new Error(`Seller with ID ${sellerId} not found`);
+    } else {
+        seller.isFavorite = false;
+    }
+
+    return seller;
+}
+
+export async function addBlockedSeller(sellerId: number): Promise<Seller | null> {
+    const seller = sellerData.find(s => s.id === sellerId);
+    if (!seller){
+        throw new Error(`Seller with ID ${sellerId} not found`);
+    } else {
+        seller.isBlocked = true;
+    }
+
+    return seller;
+}
+
+export async function removeBlockedSeller(sellerId: number): Promise<Seller | null> {
+    const seller = sellerData.find(s => s.id === sellerId);
+    if (!seller){
+        throw new Error(`Seller with ID ${sellerId} not found`);
+    } else {
+        seller.isBlocked = false;
+    }
+
     return seller;
 }
