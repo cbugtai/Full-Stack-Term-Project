51cbc1f7049b674689fac81858b73ff3e9a14832
cbugtai <cbugtai@academic.rrc.ca>
2025-10-24 09:43:08 -0500

    updated sellerRepo.ts to only contain necessary requests

--- src/apis/sellers/sellerRepo.ts
+++ src/apis/sellers/sellerRepo.ts
@@ -1,54 +1,26 @@
 import type { Seller } from "@/types/sellerModel";
 import { sellerData } from "./mockSellerData";
 
-export function fetchSellers(): Seller[] {
+export async function fetchAllSellers(): Promise<Seller[]> {
     return sellerData;
 }
 
-export function getSellerById(id: number): Seller {
-    const found = sellerData.find(s => s.id === id);
-    if (!found) {
-        throw new Error(`Seller with ID ${id} not found`);
+export async function toggleFavoriteSeller(sellerId: number): Promise<Seller | null> {
+    const seller = sellerData.find(s => s.id === sellerId);
+    if (!seller){
+        throw new Error(`Seller with ID ${sellerId} not found`);
+    } else {
+        seller.isFavorite = !seller.isFavorite;
     }
-    return found;
-}
+    return seller;
+}  
 
-export async function addSeller(newSeller: Seller) {
-    const exists = sellerData.some(s => s.id === newSeller.id);
-    if (exists) {
-        throw new Error(`Seller with ID ${newSeller.id} already exists`);
+export async function toggleBlockedSeller(sellerId: number): Promise<Seller | null> {
+    const seller = sellerData.find(s => s.id === sellerId);
+    if (!seller){
+        throw new Error(`Seller with ID ${sellerId} not found`);
+    } else {
+        seller.isBlocked = !seller.isBlocked;
     }
-    sellerData.push(newSeller);
-}
-
-export async function updateSeller(updated: Seller) {
-    const index = sellerData.findIndex(s => s.id === updated.id);
-    if (index === -1) {
-        throw new Error(`Seller with ID ${updated.id} not found`);
-    }
-    sellerData[index] = updated;
-}
-
-export async function addfavoriteSeller(sellerId: number) {
-    const seller = getSellerById(sellerId);
-    seller.isFavorite = true;
-    return seller;
-}
-
-export async function removeFavoriteSeller(sellerId: number) {
-    const seller = getSellerById(sellerId);
-    seller.isFavorite = false;
-    return seller;
-}
-
-export async function blockSeller(sellerId: number) {
-    const seller = getSellerById(sellerId);
-    seller.isBlocked = true;
-    return seller;
-}
-
-export async function unblockSeller(sellerId: number) {
-    const seller = getSellerById(sellerId);
-    seller.isBlocked = false;
     return seller;
 }
