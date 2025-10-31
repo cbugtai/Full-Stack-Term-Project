42774c265e601f640ea92efc43b61a30f00baade
pinyirao <prao2@rrc.ca>
2025-10-31 06:44:03 -0500

    feature: manage the timeout in useLoading hook

--- src/hooks/useLoading.ts
+++ src/hooks/useLoading.ts
@@ -1,19 +1,38 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // this hook manages loading state, if your page is loading data in asynchronously, you can use this hook to manage the loading state and control the loading placeholder display
 export function useLoading() {
   const [loading, setLoading] = useState(false);
 
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
   // start: sets loading to true
-  const start = () => setLoading(true);
+
+  const start = () => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+      timerRef.current = null;
+    }
+    setLoading(true);
+  };
 
   // stop: sets loading to false
   /*for demonstrattion purpose to simulate delay for data loading */
-  // const stop = () =>
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 1000);
+  const stop = () => {
+    timerRef.current = setTimeout(() => {
+      setLoading(false);
+      timerRef.current = null;
+    }, 200);
+  };
 
-  const stop = () => setLoading(false);
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, []);
+
+  // const stop = () => setLoading(false);
   return { loading, start, stop };
 }
