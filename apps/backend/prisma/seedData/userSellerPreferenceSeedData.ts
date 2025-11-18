d23ba2769ea2b6b564886ee9f7850c1d7c106008
cbugtai <cbugtai@academic.rrc.ca>
2025-11-17 18:18:03 -0600

    added seed data for prisma db tables

---
+++ apps/backend/prisma/seedData/userSellerPreferenceSeedData.ts
@@ -0,0 +1,4 @@
+export const userSellerPreferencesData = [
+  { userId: 1, sellerId: 1, isFavorite: true,  isBlocked: false },
+  { userId: 1, sellerId: 2, isFavorite: false, isBlocked: true  },
+];
