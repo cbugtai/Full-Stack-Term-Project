e5b9bf76d3572cf47c3c77123ea1b55bdc7cb92b
cbugtai <cbugtai@academic.rrc.ca>
2025-12-05 22:02:31 -0600

    fix: delete unused, broken file

--- apps/frontend/src/services/listingService.ts
+++
@@ -1,7 +0,0 @@
-import { updateListing } from "@/apis/listing/listingRepo";
-
-export const listingService = {
-    async markAsSold(id: string) {
-        return updateListing(id, { status: "sold" });
-    }
-};
